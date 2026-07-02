3569db075fc2bda6b2a8f9ebbe0dd5120b2ffd67
IgorNLoureiro <igornobrega20@gmail.com>
2026-07-02 15:11:40 -0400

    feat: create hero section component structure

---
+++ app/_components/content.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export  function Homecontent() {
+    return (
+        <>
+        {/* header */}
+        
+        
+        <main className="container mx-auto px-4 py-8">
+            <div className="grid lg:grid-cols-2 gap-8 lg:gap-12 items-center min-h-[calc(100vh-200px)]">
+                <div className="flex justify-center">
+                    {/* HERO */}
+                    aaaaaaaaaaaaaaaa
+                </div>
+                <div className="flex justify-center">
+                    {/* FOTO */}
+                    bbbbbbbbbbbbb
+                </div>
+            </div>
+        </main>
+        </>
+    )
+}
